ec0975eb54311e37bc67904b1107c1eda18004df
harry <122640493@qq.com>
2025-03-09 18:27:50 +0800

    A2B with n=8, k=32, parallel CSA

---
+++ A2Bn8/A2Bn8.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+module A2Bn8(
+    input clk_i,
+    input rst_ni,
+    input i_dvld,
+    input i_rvld,
+    input [11615:0] i_n,
+    input [255:0] i_a,
+    output [255:0] o_z,
+    output o_dvld
+	);
+
+
+endmodule
